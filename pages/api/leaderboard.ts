6acf9072a93f60c153a3f0613532bf5456d7f8d7
HX <122723004+Cantro93@users.noreply.github.com>
2026-04-20 19:41:52 +0200

    Create leaderboard API route

---
+++ pages/api/leaderboard.ts
@@ -0,0 +1,8 @@
+// leaderboard.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const leaderboard = (req: NextApiRequest, res: NextApiResponse) => {
+  // Logic for leaderboard API
+};
+
+export default leaderboard;
